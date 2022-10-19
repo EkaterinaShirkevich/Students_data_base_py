3c163f0df3bd52c4b8895342f540e410d26cd288
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-10-19 19:43:11 +0300

    a little changes #1

---
+++ user_interface/interface.py
@@ -0,0 +1,29 @@
+'''
+def main_menu():
+	
+
+
+def export_menu():
+	
+
+def import_menu():
+	
+
+
+def error():
+	print('-' * 50)
+	print('------ Ошибка -------')
+	print('-' * 50)
+
+
+def menu_contact_actions():
+	
+
+
+def exit_menu():
+	print('=' * 50)
+	print('Выберите дальнейшее действие')
+	print('-' * 50)
+	print('1. Выход в главное меню')
+	print('2. Выход из программы')
+'''
